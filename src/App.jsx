e885906745bd478bc30187b40499a17688bfb688
Bhaskar Gupta <codeforeducations@gmail.com>
2024-09-08 20:05:21 +0530

    lazy loading

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { Suspense, lazy, useState, useEffect } from 'react';
+import './App.css';
+import PropTypes from 'prop-types'; 
+
+// Lazy loading
+const MyComponent = lazy(()=>import('./MyComponent'));
+
+// const MyComponent = lazy(()=>
+//   new Promise((resolve)=>{
+//     setTimeout(()=> resolve(import('./MyComponent')), 5000);
+//   })
+// )
+
+// Error Boundary
+const ErrorBoundary = ({children}) => {
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(()=>{
+    const handleError = () => setHasError(true);
+    window.addEventListener('error', handleError);
+
+    return () => {
+      window.removeEventListener('error', handleError);
+    }
+  },[])
+
+  return hasError ? <div>Error loading component.</div> : children;
+}
+
+// Define
+ErrorBoundary.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+function App() { 
+  return (
+    <>
+      <div>
+      <h1>Lazy App</h1> 
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+        <MyComponent />
+        </Suspense>
+      </ErrorBoundary> 
+    </div>
+    </>
+  )
+}
+
+export default App
